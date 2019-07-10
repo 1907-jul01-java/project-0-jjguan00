729f6974bd72551c3831edc7b8ad926957776a70
Jackie <jjguan00@gmail.com>
2019-07-09 23:10:45 -0500

    add menu option with scanner

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,4 +1,7 @@
 package com.revature;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -6,6 +9,18 @@ package com.revature;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        System.out.println("Welcome to Bank, Please choose from the following menu.");
+        System.out.println("Sign up, Log in");
+        List<String> commands = new ArrayList<String> ();
+        commands.add("Sign Up");
+        commands.add("Log In");
+		Scanner sc = new Scanner(System.in);
+		String command = sc.nextLine();
+		if(commands.contains(command)) {
+			System.out.print(command);
+			}
+		else {
+			System.out.println("Please enter a proper command");
+		}
+		}
 }
