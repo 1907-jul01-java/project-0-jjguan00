0a292f89a775d15dc2e5f6a25bb6f3deda45ce40
Jackie <jjguan00@gmail.com>
2019-07-14 21:53:08 -0500

    revert back

---
+++ bin/db/schema.sql
@@ -0,0 +1,40 @@
+drop table if exists checkusers;
+drop table if exists users;
+drop table if exists checks;
+drop table if exists admins;
+
+create table users (
+  id serial primary key,
+  username text not null unique,
+  firstname text not null,
+  password text not null
+);
+
+create table checks(
+	id int primary key,
+	balance int not null,
+	approved boolean not null
+);
+
+CREATE TABLE checkusers
+(
+userID INT NOT NULL, 
+checkAccount INT NOT NULL,
+PRIMARY KEY(userID, checkAccount),
+
+constraint fk_checkuser__user
+foreign key(userID) references Users(id),
+
+constraint fk_checkuser__checkID
+foreign key(checkAccount) references Checks(id)
+
+);
+
+create table admins(
+	id serial primary key,
+	adminname text not null,
+	password text not null
+);
+
+INSERT INTO admins(adminname,password) VALUES ('admin','admin');
+INSERT INTO checks values(11111, '1000', false);
